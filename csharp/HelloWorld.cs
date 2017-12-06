1f7c7f8fa1f29525f00c1fa8baec655d41d47716
Glenn Lanzer <lanzertrey@gmail.com>
2017-12-05 21:29:39 -0600

    create: HelloWorld.cs

---
+++ csharp/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace helloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
